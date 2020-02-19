63e31bd57a5553e30c46266229b102d027ea8b47
Jeremy Nelson <jermnelson@gmail.com>
2020-02-19 16:07:12 -0700

    Passing Flake8, pytest, and started support for mypy

--- rdf_classify/extract.py
+++ rdf_classify/extract.py
@@ -1,32 +1,32 @@
-"""Extracts serialized RDF and loads into one or more graphs""" 
+"""Extracts serialized RDF and loads into one or more graphs"""
 
 import json
-import rdflib
+import rdflib  # type: ignore
 from zipfile import ZipFile
 
 
-def from_zipfile(zip_filepath, exclude=[]):
-  """Takes a zip filepath, extracts Sinopia RDF files, loading each JSON-LD
-  into a rdflib.ConjunctionGraph, returns a list of these graphs.
+def from_zipfile(zip_filepath: str, exclude: list = []) -> list:
+    """Takes a zip filepath, extracts Sinopia RDF files, loading each JSON-LD
+    into a rdflib.ConjunctionGraph, returns a list of these graphs.
 
-  @param zip_filepath -- Path to zip files
-  @param excludes -- Directory of file patterns to match on the zip file. Used
-                     to filter out non-RDF payloads
-  """
-  graphs = []
-  with ZipFile(zip_filepath) as zip_file:
-    for zip_info in zip_file.infolist():
-      if zip_info.file_size < 1:
-        continue
-      if zip_info.filename.split('/')[1] in exclude:
-        continue
-      with zip_file.open(zip_info) as zip_extract:
-        graph = rdflib.ConjunctiveGraph()
-        raw_rdf = zip_extract.read()
-        try:
-          graph.parse(data=raw_rdf, format='json-ld')
-          graphs.append(graph)
-        except json.JSONDecodeError:
-          print(f"Failed to parse {zip_info.filename}")
-          continue
-  return graphs
+    @param zip_filepath -- Path to zip files
+    @param excludes -- Directory of file patterns to match on the zip file.
+                       Used to filter out non-RDF payloads.
+    """
+    graphs = []
+    with ZipFile(zip_filepath) as zip_file:
+        for zip_info in zip_file.infolist():
+            if zip_info.file_size < 1:
+                continue
+            if zip_info.filename.split('/')[1] in exclude:
+                continue
+            with zip_file.open(zip_info) as zip_extract:
+                graph = rdflib.ConjunctiveGraph()
+                raw_rdf = zip_extract.read()
+                try:
+                    graph.parse(data=raw_rdf, format='json-ld')
+                    graphs.append(graph)
+                except json.JSONDecodeError:
+                    print(f"Failed to parse {zip_info.filename}")
+                    continue
+    return graphs
--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,6 +1,7 @@
 from rdf_classify import extract
 
+
 class TestExtract:
 
-  def test_from_zipfile(self):
-    assert hasattr(extract, 'from_zipfile')
+    def test_from_zipfile(self):
+        assert hasattr(extract, 'from_zipfile')
