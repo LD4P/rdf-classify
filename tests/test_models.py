4cc675ad3487d83737904430cf5ef598f13e0d1f
Jeremy Nelson <jermnelson@gmail.com>
2020-06-29 17:13:16 -0600

    Refactoring to use FastAI

--- tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from rdf_classify import model
-
-
-class TestModel:
-    def test_fastforward(self):
-        basic_model = model.compile_feedforward(10)
-        print(basic_model)
-        assert 1
