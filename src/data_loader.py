441714740aa6c6b4e78396cedca8c026a1935149
Jeremy Nelson <jermnelson@gmail.com>
2020-10-15 11:56:23 -0600

    Added new reports and function for harvesting resources from the new Sinopia API (w/MongoDB backend)

--- src/data_loader.py
+++ src/data_loader.py
@@ -5,10 +5,11 @@ import json
 import rdflib  # type: ignore
 import requests  # type: ignore
 from datetime import datetime
-from typing import List, Optional
+from typing import Dict, List, Optional
 from zipfile import ZipFile
 
 LDP = rdflib.Namespace("http://www.w3.org/ns/ldp#")
+SINOPIA = rdflib.Namespace("http://sinopia.io/vocabulary/")
 
 
 def create_tensor(graph: rdflib.Graph, group: str) -> List:
@@ -22,29 +23,31 @@ def create_tensor(graph: rdflib.Graph, group: str) -> List:
 
 
 def predicate_columns(resource_template_group_url: str) -> List:
-    properties: List[str] = ['subject', 'resource_template', 'group']
+    properties: List[str] = ["subject", "resource_template", "group"]
     ld4p_graph = rdflib.Graph()
     ld4p_result = requests.get(resource_template_group_url)
-    ld4p_graph.parse(data=ld4p_result.text, format='turtle')
+    ld4p_graph.parse(data=ld4p_result.text, format="turtle")
     for rt_url in ld4p_graph.objects(predicate=LDP.contains):
         rt_result = requests.get(str(rt_url))
         if rt_result.status_code > 300:
             print(f"Failed {rt_url} status {rt_result.status_code}")
             continue
         for row in rt_result.json()["propertyTemplates"]:
-            property_url = row.get('propertyURI')
+            property_url = row.get("propertyURI")
             if property_url not in properties:
                 properties.append(property_url)
     return properties
 
 
-def process_predicates(subject_key: str,
-                       predicates: List,
-                       resource_template: Optional[rdflib.URIRef],
-                       group: str) -> dict:
-    data = {'subject': subject_key, 'group': group}
+def process_predicates(
+    subject_key: str,
+    predicates: List,
+    resource_template: Optional[rdflib.URIRef],
+    group: str,
+) -> dict:
+    data = {"subject": subject_key, "group": group}
     if resource_template is not None:
-        data['resource_template'] = str(resource_template)
+        data["resource_template"] = str(resource_template)
     for predicate in predicates:
         pred_key = str(predicate)
         if pred_key in data:
@@ -66,21 +69,69 @@ def to_dataframe(graphs: List[dict], rt_url: str) -> pd.DataFrame:
     raw_data = []
     predicate_cols = predicate_columns(rt_url)
     for i, row in enumerate(graphs):
-        graph = row['graph']
+        graph = row["graph"]
         if not i % 10:
             print(".", end="")
         if not i % 100:
             print(f"{i}", end="")
-        subjects = create_tensor(graph, row['group'])
+        subjects = create_tensor(graph, row["group"])
         raw_data.extend(subjects)
     end = datetime.utcnow()
-    df = pd.DataFrame(data=raw_data,
-                      columns=predicate_cols)
+    df = pd.DataFrame(data=raw_data, columns=predicate_cols)
     df = df.fillna(0).sample(frac=1).reset_index(drop=True)
-    print(f"""
+    print(
+        f"""
     Finished at {end} time {(end-start).seconds / 60.} minutes for dataframe,
-    size {len(df)}""")
+    size {len(df)}"""
+    )
     return df
+
+
+def from_api(api_url: str) -> Dict:
+    """Takes the new Sinopia API endpoint URI, extracts each resource and
+    template, and returns a dictionary with two lists, a resources and a
+    templates, and the total number of resources harvested from the api.
+
+    @param api_url -- URI to Sinopia API endpoint
+    """
+
+    def add_resource(resource):
+        output["total"] += 1
+        graph = rdflib.Graph()
+        graph.namespace_manager.bind("sinopia", SINOPIA)
+        jsonld = json.dumps(resource.pop("data")).encode()
+        graph.parse(data=jsonld, format="json-ld")
+        payload = {"graph": graph, "meta": resource}
+        if "sinopia:template:resource" in resource.get("templateId"):
+            output["templates"].append(payload)
+        else:
+            output["resources"].append(payload)
+
+    output = {"resources": [], "templates": [], "total": 0}
+    start = datetime.utcnow()
+    print(f"Started harvest of resources at {start} for {api_url}")
+    initial = requests.get(f"{api_url}/resource")
+    print("0", end="")
+    for row in initial.json().get("data"):
+        add_resource(row)
+    next_link = initial.json().get("links").get("next")
+    while 1:
+        result = requests.get(next_link)
+        if result.status_code > 300:
+            break
+        payload = result.json()
+        new_next = payload.get("links").get("next")
+        if new_next == next_link or new_next is None:
+            break
+        for row in payload.get("data"):
+            add_resource(row)
+        next_link = new_next
+        print(".", end="")
+        if not output["total"] % 250:
+            print(f"{output['total']}", end="")
+    end = datetime.utcnow()
+    print(f"\nFinished at {end}, total time {(end-start).seconds / 60.}")
+    return output
 
 
 def from_zipfile(zip_filepath: str) -> List:
@@ -102,8 +153,8 @@ def from_zipfile(zip_filepath: str) -> List:
                 graph = rdflib.ConjunctiveGraph()
                 try:
                     graph.parse(data=raw_data, format="json-ld")
-                    graph.skolemize(
-                        authority=f"https://{graph.identifier}.sinopia.io/")
+                    authority = f"https://{graph.identifier}.sinopia.io/"
+                    graph.skolemize(authority=authority)
                     graphs.append({"group": group, "graph": graph})
                 except json.JSONDecodeError:
                     print(f"Failed to parse {zip_info.filename}")
--- src/data_prep.py
+++ src/data_prep.py
@@ -6,23 +6,29 @@ from fastai.tabular import Categorify, DataBunch, TabularList  # type: ignore
 
 
 def databunch(df: pd.DataFrame,
-              dependent_var: str = 'resource_template') -> DataBunch:
+              dependent_var: str = "resource_template") -> DataBunch:
     # Resource_template is what we're trying to predict
-    category_names = ['subject', 'group']
+    category_names = ["subject", "group"]
     procedures = [Categorify]
     # All predicates in graph
     continous_names = list(df.keys())[3:]
     # Reserve last 20% of Data Frame for validation
     total = len(df)
-    last_start = total - int(total*.2)
-    test = TabularList.from_df(df[last_start:total].copy(),
-                               cat_names=category_names,
-                               cont_names=continous_names)
-    return (TabularList.from_df(df,
-                                cat_names=category_names,
-                                cont_names=continous_names,
-                                procs=procedures)
-                       .split_by_idx(list(range(last_start, total)))
-                       .label_from_df(cols=dependent_var)
-                       .add_test(test)
-                       .databunch())
+    last_start = total - int(total * 0.2)
+    test = TabularList.from_df(
+        df[last_start:total].copy(),
+        cat_names=category_names,
+        cont_names=continous_names,
+    )
+    return (
+        TabularList.from_df(
+            df,
+            cat_names=category_names,
+            cont_names=continous_names,
+            procs=procedures
+        )
+        .split_by_idx(list(range(last_start, total)))
+        .label_from_df(cols=dependent_var)
+        .add_test(test)
+        .databunch()
+    )
